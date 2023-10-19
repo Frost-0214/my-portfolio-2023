650b421af17ebdc1bff06f9bafd0e98e750045d5
Frost-0214 <jmgasangue@gmail.com>
2023-10-19 18:50:03 +0800

    Update and Fixes

--- src/pages/certificates.js
+++ src/pages/certificates.js
@@ -115,13 +115,13 @@ const certificates = () => {
                     img={diploma}
                     title="Diploma"
                     time="July 27, 2023"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FDiploma.pdf?alt=media&token=882df292-0081-4c65-b811-a192f7663cd3&_gl=1*1k1498i*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjEwMDcuMzUuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FDiploma.pdf?alt=media&token=41ac88fa-81df-4649-ab1b-24b5c784af35&_gl=1*18ue4pq*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTE1MTkuNjAuMC4w"
                     />
                     <FeaturedCertificate
                     img={studentleaders}
                     title="Student Leaders Council"
                     time="July 24, 2023"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FStudent-Leaders-Council.pdf?alt=media&token=60980f13-40ba-4180-9f5c-6c6762f34b6f&_gl=1*7t7vhe*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjA5OTQuNDguMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FStudent-Leaders-Council.pdf?alt=media&token=ef949a83-29bf-41ff-b8da-d1b434cc27b8&_gl=1*11jj3a0*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTE1NzEuOC4wLjA."
                     />
                 </ul>
                 <h2 className='font-bold text-4xl w-full text-center my-16 mt-32'>All Certificates</h2>
@@ -129,97 +129,97 @@ const certificates = () => {
                     <Article
                     title="3rd SDCA International Virtual Multidisciplinary Research Conference 2023"
                     date="April 18, 2023"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FREL.pdf?alt=media&token=b3d4b3d1-033c-47e7-9d1b-065745b17c83&_gl=1*87i8go*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjExNzIuNTAuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FREL.pdf?alt=media&token=87e328cc-35a6-442d-8238-6b8f0aa76784&_gl=1*1xd8eis*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTE1OTQuNjAuMC4w"
                     img={rel}
                     />
                     <Article
                     title="Test of English for International Communication 2023"
                     date="April 12, 2023"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FTOEIC-2023.pdf?alt=media&token=dc798464-7023-4e48-a144-132891d86f9e&_gl=1*d04dy4*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE1NDkuMTEuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FTOEIC-2023.pdf?alt=media&token=6d05523c-3084-408b-afea-cf47373bb6d2&_gl=1*ryidog*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTE2MTcuMzcuMC4w"
                     img={toeic2023}
                     />
                     <Article
                     title="Cultural Center of the Philippines: Internship Program at the Management Services Division"
                     date="December 13, 2022"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FGasangue%2C%20John%20Martin%20M.%20OJT%20Certificate.pdf?alt=media&token=46ae64ed-cc97-4050-9da7-00d865b90ea5&_gl=1*dhonjx*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE2NTcuNDYuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FGasangue%2C%20John%20Martin%20M.%20OJT%20Certificate.pdf?alt=media&token=32ca0c0f-769d-4821-b091-cacbef8b7cf0&_gl=1*bl02om*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTE2MzIuMjIuMC4w"
                     img={ojt}
                     />
                     <Article
                     title="Dean's Lister 2nd Semester A.Y. 2021-2022"
                     date="November 28, 2022"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FDL-Second-Semester.pdf?alt=media&token=b28cb58c-da5d-4dd1-b8e1-cf8254a3da4b&_gl=1*prly6y*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE1NjIuNjAuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FDL-Second-Semester.pdf?alt=media&token=661a1df5-a651-4e95-8873-f1fefd032b0c&_gl=1*1l8eoym*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTIyMzkuNjAuMC4w"
                     img={dl2}
                     />
                     <Article
                     title="Dean's Lister 1st Semester A.Y. 2021-2022"
                     date="November 28, 2022"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FDL-First-Semester.pdf?alt=media&token=0fbe1fc6-8414-4051-842a-307a01242906&_gl=1*12sxzpi*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE1NzEuNTEuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FDL-First-Semester.pdf?alt=media&token=d3e07235-ea80-422c-8441-aa45d775cdb4&_gl=1*p209wb*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTIyOTQuNS4wLjA."
                     img={dl1}
                     />
                     <Article
                     title="Webinar on Data Privacy Awareness"
                     date="November 16, 2022"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FJohn%20Martin%20M.%20Gasangue%20%20-%20Certificate%20of%20Attendance.pdf?alt=media&token=2c54858f-8283-449d-81a1-dad665181a59&_gl=1*1dr6975*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE4NDMuNjAuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FJohn%20Martin%20M.%20Gasangue%20%20-%20Certificate%20of%20Attendance.pdf?alt=media&token=424f771a-5487-411b-98ba-be54800da065&_gl=1*12007au*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTIzMTIuNjAuMC4w"
                     img={wdpa}
                     />
                     <Article
                     title="18th Cinemalaya Philippine Independent Film Festival 2022"
                     date="August 19, 2022"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FCinemalaya18.pdf?alt=media&token=03a25391-6c49-4a34-8e58-0ae5e1072a3a&_gl=1*12nlg3z*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE2NzguMjUuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FCinemalaya18.pdf?alt=media&token=002b7db1-8159-44b8-b6ec-c01654838c92&_gl=1*11zgil6*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTIzMjQuNDguMC4w"
                     img={cinemalaya18}
                     />
                     <Article
                     title="Test of English for International Communication 2022"
                     date="February 18, 2022"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FTOEIC-2022.pdf?alt=media&token=44cc64ed-08c2-47bc-b5f1-4e23fd45ed8f&_gl=1*14djpr3*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE2OTAuMTMuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FTOEIC-2022.pdf?alt=media&token=55d82dc1-099d-4f12-9b8d-eebafe668b9f&_gl=1*1i11tu9*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTIzMzUuMzcuMC4w"
                     img={toeic2022}
                     />
                     <Article
                     title="JavaScript Essential Training (2017)"
                     date="December 13, 2021"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FCertificateOfCompletion_JavaScript%20Essential%20Training%202017.pdf?alt=media&token=14f7a1c1-7b97-4f47-a4e3-4e4012d01244&_gl=1*tc9f1t*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE5MDMuNjAuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FCertificateOfCompletion_JavaScript%20Essential%20Training%202017.pdf?alt=media&token=ead2f478-5473-427c-9cc4-8e49392e60f2&_gl=1*13fubpf*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTIzNzMuNjAuMC4w"
                     img={jscert}
                     />
                     <Article
                     title="CSS Essential Training"
                     date="November 2, 2021"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FCertificateOfCompletion_CSS%20Essential%20Training.pdf?alt=media&token=07ab08d3-c21b-4f89-a7d7-23371edcf190&_gl=1*5zelc1*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE5MjkuMzQuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FCertificateOfCompletion_CSS%20Essential%20Training.pdf?alt=media&token=ed117322-5ae5-4b4a-b832-cd7941d33217&_gl=1*sxhk4g*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTIzODEuNTIuMC4w"
                     img={csscert}
                     />
                     <Article
                     title="HTML Essential Training"
                     date="September 28, 2021"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FCertificateOfCompletion_HTML%20Essential%20Training.pdf?alt=media&token=00b1c938-6b72-47c3-8ffe-a5b37336d76f&_gl=1*xgy8qn*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE5MzUuMjguMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FCertificateOfCompletion_HTML%20Essential%20Training.pdf?alt=media&token=5836d6d0-a30c-4fc5-90fc-87dcc9a028f5&_gl=1*1d9fjp7*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTIzODkuNDQuMC4w"
                     img={htmlcert}
                     />
                     <Article
                     title="WOCEE Wired TechTalk Webinar 2021: Digital Technology Maximization"
                     date="August 28, 2021"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FTECHTALK%20DAY%203%20E-CERT.pdf?alt=media&token=9626897c-1d4b-4145-bfc8-e7fbae54b1ce&_gl=1*1p3ab0y*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE5ODQuNjAuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FTECHTALK%20DAY%203%20E-CERT.pdf?alt=media&token=cd343965-15be-45e0-be52-74576b1d46d1&_gl=1*1kvfjun*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTI0ODUuNDEuMC4w"
                     img={digital}
                     />
                     <Article
                     title="WOCEE Wired TechTalk Webinar 2021: Robotics and Machine Intelligence"
                     date="August 27, 2021"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FWocee%20Wired%20TechTalk%20Webinar%20E-Cert%20Day%202.pdf?alt=media&token=f656428f-75bd-4fe8-865c-6f566ce04cda&_gl=1*1ke0sbk*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjE5OTcuNDcuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FWocee%20Wired%20TechTalk%20Webinar%20E-Cert%20Day%202.pdf?alt=media&token=b6b7177f-0285-4e9f-a26a-763f3ab5e185&_gl=1*ejwos6*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTI1MDMuMjMuMC4w"
                     img={robot}
                     />
                     <Article
                     title="WOCEE Wired TechTalk Webinar 2021: Blockchain and the Cloud"
                     date="August 26, 2021"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FWocee%20Wired%20TechTalk%20Webinar%20E-Cert%20Day%201.pdf?alt=media&token=5c7305a3-67cd-4d14-9adb-03297b8f90f6&_gl=1*tunrts*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjIwMDguMzYuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FWocee%20Wired%20TechTalk%20Webinar%20E-Cert%20Day%201.pdf?alt=media&token=1ac49781-6222-4b10-9301-b6a83eac3c97&_gl=1*17zodrk*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTI1MTEuMTUuMC4w"
                     img={blockchain}
                     />
                     <Article
                     title="Microsoft Office Specialist: Office PowerPoint 2016"
                     date="March 29, 2019"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FOffice%20PowerPoint%202016.pdf?alt=media&token=7e429085-27c0-4fa2-957e-f3ff50ed5637&_gl=1*1wdbs9j*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjIwNTAuNjAuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FOffice%20PowerPoint%202016.pdf?alt=media&token=317cfbe7-8f5d-4909-9acf-3b735038befc&_gl=1*12ogs3b*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTI1MzguNjAuMC4w"
                     img={ppt}
                     />
                     <Article
                     title="Microsoft Office Specialist: Office Word 2016"
                     date="November 20, 2018"
-                    link="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/certificates%2FOffice%20Word%202016.pdf?alt=media&token=fbd19662-307a-4230-81ce-3fc1a891a844&_gl=1*kzos3k*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NjM2MDQ3NC42Mi4xLjE2OTYzNjIwNjcuNDMuMC4w"
+                    link="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/certificates%2FOffice%20Word%202016.pdf?alt=media&token=27ab7e19-7738-45cd-8f04-50779188bb51&_gl=1*s5rfl8*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5NzcxMDg0Ni43OS4xLjE2OTc3MTI1NDUuNTMuMC4w"
                     img={word}
                     />
                 </ul>
