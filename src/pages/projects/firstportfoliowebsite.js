08eaa644c2c0b7e2599f629f0b48808ed1d45181
Frost-0214 <jmgasangue@gmail.com>
2023-10-27 16:27:22 +0800

    Update for firstportfoliowebsite.js

--- src/pages/projects/firstportfoliowebsite.js
+++ src/pages/projects/firstportfoliowebsite.js
@@ -4,13 +4,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react'
 import headimg from "../../../public/images/projects/first-portfolio.png"
-import home from "../../../public/images/projects/HSPA/home.png"
-import search from "../../../public/images/projects/HSPA/search.png"
-import profile from "../../../public/images/projects/HSPA/profile.png"
-import hspa2 from "../../../public/images/projects/HSPA/hspa-2.png"
-import hspaend from "../../../public/images/projects/HSPA/hspa-end.png"
-import psimg from "../../../public/images/projects/HSPA/ps-button.png"
-import ip14 from "../../../public/images/devices/ip14.png"
+import skills from "../../../public/images/projects/FPW/skills.png"
+import services from "../../../public/images/projects/FPW/services.png"
+import project from "../../../public/images/projects/FPW/project.png"
+import testimonial from "../../../public/images/projects/FPW/testimonial.png"
 import sdcacalculator from "../../../public/images/projects/sdca-calculator-cropped.png"
 import { motion } from 'framer-motion'
 import Link from 'next/link'
@@ -57,63 +54,40 @@ const firstportfoliowebsite = () => {
             </div>
           </Layout>
           <div className='col-span-12'>
-            <video src="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/vids%2FHSPA.mp4?alt=media&token=d9bb2f64-7ab1-4905-be11-01b35f543322&_gl=1*t52n19*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5Njc4MzEwMC42My4xLjE2OTY3ODMxOTEuNjAuMC4w" muted loop autoPlay priority className='w-full h-auto'/>
-          </div>
-          <Layout className='pt-16'>
-            <div className='grid grid-cols-3 mt-16'>
-                <FramerImage src={home} alt="JohnMartin" className='w-full h-auto'
-                priority
-                initial={{y:150, opacity:1}}
-                whileInView={{y:0, opacity:1, transition:{duration:0.5}}}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
-                />
-                <FramerImage src={search} alt="JohnMartin" className='w-full h-auto'
-                priority
-                initial={{y:150, opacity:1}}
-                whileInView={{y:0, opacity:1, transition:{duration:0.5}}}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
-                />
-                <FramerImage src={profile} alt="JohnMartin" className='w-full h-auto'
-                priority
-                initial={{y:150, opacity:1}}
-                whileInView={{y:0, opacity:1, transition:{duration:0.5}}}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
-                />
-            </div>
-          </Layout>
-          <div className='col-span-12 mt-16'>
-                <Image src={hspa2} alt="JohnMartin" className='w-full h-auto'
-                priority
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
-                />
+            <video src="https://firebasestorage.googleapis.com/v0/b/myportfolio-794c9.appspot.com/o/videos%2Ffirstportfolio.mp4?alt=media&token=fead658a-b0af-4b18-8409-b9c0776a05da&_gl=1*op6rce*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5ODM5MjY2Ny44NC4xLjE2OTgzOTUxNjAuMTQuMC4w" muted loop autoPlay priority className='w-full h-auto'/>
           </div>
           <Layout className='mt-16'>
-            <div className='flex relative gap-24 gap-y-32 mt-4 mb-4 items-center justify-center'>
-              <div className='items-center justify-center'>
-                <video src="https://firebasestorage.googleapis.com/v0/b/portfolio-99e6e.appspot.com/o/vids%2FHSPA-Booking.mp4?alt=media&token=7a9d2944-22ad-4989-90e6-4801a6100e13&_gl=1*3q54x3*_ga*MTgyMTI4ODg0MC4xNjczOTg0Njc0*_ga_CW55HF8NVT*MTY5Njg3MzI4NS42NS4xLjE2OTY4NzQyOTcuNDAuMC4w" 
-                muted 
-                loop 
-                autoPlay 
-                priority 
-                className='w-[25.8%] h-auto absolute top-11 ml-44 rounded-3xl -z-10'/>
-                <Image src={ip14} alt="JohnMartin" className='w-full h-auto'
-                priority
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 45vw, 45vw"
-                />
-              </div>
-            </div>
+            <div className='grid grid-cols-2 gap-16'>
+                    <FramerImage src={skills} alt="JohnMartin" className='w-full h-auto overflow-hidden'
+                    priority
+                    initial={{y:150, opacity:0}}
+                    whileInView={{y:0, opacity:1, transition:{duration:0.5}}}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
+                    />
+                    <FramerImage src={services} alt="JohnMartin" className='w-full h-auto'
+                    priority
+                    initial={{y:150, opacity:0}}
+                    whileInView={{y:0, opacity:1, transition:{duration:0.5}}}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
+                    />
+                </div>
           </Layout>
-          <div className='col-span-12 mt-16'>
-            <Image src={hspaend} alt="JohnMartin" className='w-full h-auto'
-            priority
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
-            />
-              <Link href="https://play.google.com/store/apps/details?id=com.hspa.capstone&pcampaignid=web_share" target='_blank'>
-                <Image src={psimg} alt="JohnMartin" className='w-[42%] h-auto absolute ml-24 -mt-80 cursor-pointer'
-                priority
-                />
-              </Link>
-          </div>
+          <Layout className='mt-16'>
+            <div className='grid grid-cols-2 gap-16'>
+                    <FramerImage src={project} alt="JohnMartin" className='w-full h-auto overflow-hidden'
+                    priority
+                    initial={{y:150, opacity:0}}
+                    whileInView={{y:0, opacity:1, transition:{duration:0.5}}}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
+                    />
+                    <FramerImage src={testimonial} alt="JohnMartin" className='w-full h-auto'
+                    priority
+                    initial={{y:150, opacity:0}}
+                    whileInView={{y:0, opacity:1, transition:{duration:0.5}}}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
+                    />
+                </div>
+          </Layout>
           <Layout className='py-16'>
             <motion.h2 initial={{y:100, opacity:0}}
             whileInView={{y:0, opacity:1, transition:{duration:1, delay:0.4}}}
